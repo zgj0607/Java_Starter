2ed8de9758bcb5f6ca6d54ed5d1eda654b209745
Jimmy Zhou <zgj0607@gmail.com>
2016-10-13 10:58:05 +0800

    version initial

---
+++ src/main/java/com/restree/constant/ResourceType.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016
+ * Just for learn java coding
+ */
+
+package com.restree.constant;
+
+public class ResourceType {
+    //BRAS
+    public static final int BRAS = 2406;
+
+    //交换机
+    public static final int SWITCH = 901;
+
+    //OLT
+    public static final int OLT = 2510;
+
+    //OBD
+    public static final int OBD = 2530;
+
+    //ONU
+    public static final int ONU = 2511;
+
+    //用户终端
+    public static final int TERMINAL = 2508;
+
+    //ONT
+    public static final int ONT = 2736;
+
+    //ODF
+    public static final int ODF = 411;
+
+    //光交接箱
+    public static final int CONNECT_BOX = 703;
+
+    //光分线盒
+    public static final int JOIN_BOX = 704;
+
+    //光缆接头
+    public static final int TIE_IN = 705;
+
+    //综合配线箱
+    public static final int PXX = 414;
+
+    //机架
+    public static final int RACK = 302;
+
+    //光缆预留
+    public static final int PRE_HOLD = 737;
+
+    //FTTB
+    public static final String FTTB = "FTTB";
+
+    public static final String FTTB_DICT = "5";
+
+    //FTTH
+    public static final String FTTH = "FTTH";
+
+    public static final String FTTH_DICT = "2";
+
+
+}
